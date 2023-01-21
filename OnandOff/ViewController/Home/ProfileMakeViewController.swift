8a74699e5ad23300cc620cb3a294a41cd93c3b7d
2022swift <engseoul22@gmail.com>
2023-01-21 15:04:00 +0900

    feature_HomeProfile

--- OnandOff/ViewController/Home/ProfileMakeViewController.swift
+++ OnandOff/ViewController/Home/ProfileMakeViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 import SnapKit
 import Then
-import SwiftUI
 
 class ProfileMakeViewController: UIViewController {
     
@@ -36,12 +35,16 @@ class ProfileMakeViewController: UIViewController {
         $0.image = UIImage(named: "ProfileInsertPhoto")
     }
     
-    let profileInsertPhotoView = UIView().then {_ in
-    }
-    
     let personaLbl = UILabel().then {
         $0.font = .notoSans(size: 13.5, family: .Bold)
         $0.text = "페르소나"
+    }
+    
+    let personaTxField = UITextField().then {
+        $0.font = .notoSans(size: 13.5, family: .Bold)
+        $0.textColor = UIColor.mainColor
+        $0.placeholder = "최대 4글자"
+        $0.addTarget(self, action: #selector(textFieldEditingChanged), for: .editingChanged)
     }
     
     let shortLine1 = UIImageView().then {
@@ -53,6 +56,13 @@ class ProfileMakeViewController: UIViewController {
         $0.text = "닉네임"
     }
     
+    let nickNameTxField = UITextField().then {
+        $0.font = .notoSans(size: 13.5, family: .Bold)
+        $0.textColor = UIColor.mainColor
+        $0.placeholder = "최대 8글자"
+        $0.addTarget(self, action: #selector(textFieldEditingChanged), for: .editingChanged)
+    }
+    
     let shortLine2 = UIImageView().then {
         $0.image = UIImage(named: "ShortLine")
     }
@@ -60,6 +70,17 @@ class ProfileMakeViewController: UIViewController {
     let introduceLbl = UILabel().then {
         $0.font = .notoSans(size: 13.5, family: .Bold)
         $0.text = "한줄소개"
+    }
+    
+    let introduceTxField = UITextField().then {
+        $0.font = .notoSans(size: 13.5, family: .Bold)
+        $0.textColor = UIColor.mainColor
+        $0.placeholder = "최대 30글자"
+        $0.addTarget(self, action: #selector(textFieldEditingChanged), for: .editingChanged)
+    }
+    
+    let longLine = UIImageView().then {
+        $0.image = UIImage(named: "LongLine")
     }
 
     override func viewDidLoad() {
@@ -81,12 +102,15 @@ class ProfileMakeViewController: UIViewController {
         view.addSubview(profileMakeLbl)
         view.addSubview(makeBtn)
         view.addSubview(profileInsertPhotoImgView)
-//        view.addSubview(profileInsertPhotoView)
-//        profileInsertPhotoImgView.addSubview(profileInsertPhotoImgView)
         view.addSubview(personaLbl)
+        view.addSubview(personaTxField)
         view.addSubview(shortLine1)
         view.addSubview(nickNameLbl)
+        view.addSubview(nickNameTxField)
         view.addSubview(shortLine2)
+        view.addSubview(introduceLbl)
+        view.addSubview(introduceTxField)
+        view.addSubview(longLine)
     }
     
     func layout() {
@@ -118,6 +142,13 @@ class ProfileMakeViewController: UIViewController {
             $0.leading.equalTo(shortLine1.snp.leading)
         }
         
+        personaTxField.snp.makeConstraints {
+            $0.height.equalTo(13.5)
+            $0.top.equalTo(personaLbl.snp.top)
+            $0.leading.equalTo(personaLbl.snp.trailing).offset(15)
+            $0.bottom.equalTo(personaLbl.snp.bottom).offset(-0.2)
+        }
+        
         shortLine1.snp.makeConstraints {
             $0.top.equalTo(personaLbl.snp.bottom).offset(6.5)
             $0.centerX.equalToSuperview().offset(-85)
@@ -128,18 +159,43 @@ class ProfileMakeViewController: UIViewController {
             $0.centerX.equalToSuperview().offset(30)
         }
         
+        nickNameTxField.snp.makeConstraints {
+            $0.height.equalTo(13.5)
+            $0.top.equalTo(nickNameLbl.snp.top)
+            $0.leading.equalTo(nickNameLbl.snp.trailing).offset(15)
+            $0.bottom.equalTo(nickNameLbl.snp.bottom).offset(-0.2)
+        }
+        
         shortLine2.snp.makeConstraints {
             $0.top.equalTo(nickNameLbl.snp.bottom).offset(6.5)
             $0.leading.equalTo(nickNameLbl.snp.leading)
         }
+        
+        introduceLbl.snp.makeConstraints {
+            $0.top.equalTo(personaLbl.snp.bottom).offset(46)
+            $0.leading.equalTo(personaLbl.snp.leading)
+        }
+        
+        introduceTxField.snp.makeConstraints {
+            $0.height.equalTo(13.5)
+            $0.top.equalTo(introduceLbl.snp.top)
+            $0.leading.equalTo(introduceLbl.snp.trailing).offset(15)
+            $0.bottom.equalTo(introduceLbl.snp.bottom).offset(-0.2)
+        }
+        
+        longLine.snp.makeConstraints {
+            $0.top.equalTo(introduceLbl.snp.bottom).offset(6.5)
+            $0.leading.equalTo(shortLine1.snp.leading)
+            $0.trailing.equalTo(shortLine2.snp.trailing)
+        }
     }
-    
+
     @objc func backToHome(sender: UIButton!) {
         self.dismiss(animated: true, completion: nil)
     }
     
     @objc func insertPhoto(sender: UIButton!) {
-        
+  
         let alert = UIAlertController(title: "", message: "", preferredStyle: .actionSheet)
         let library = UIAlertAction(title: "사진앨범", style: .default) { (action) in self.openLibrary()}
         let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
@@ -149,12 +205,22 @@ class ProfileMakeViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    func openLibrary(){
+    func openLibrary() {
 
       picker.sourceType = .photoLibrary
       present(picker, animated: false, completion: nil)
     }
-
+    
+    @objc func textFieldEditingChanged(_ textField: UITextField) {
+        
+        if personaTxField.text!.count > 4 {
+            personaTxField.deleteBackward()
+        } else if nickNameTxField.text!.count > 8 {
+            nickNameTxField.deleteBackward()
+        } else if introduceTxField.text!.count > 30 {
+            introduceTxField.deleteBackward()
+        }
+    }
 }
 
 extension ProfileMakeViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -165,12 +231,7 @@ extension ProfileMakeViewController : UIImagePickerControllerDelegate, UINavigat
             profileInsertPhotoImgView.image = img
             profileInsertPhotoImgView.layer.cornerRadius = profileInsertPhotoImgView.frame.height/2
             profileInsertPhotoImgView.clipsToBounds = true
-       
-            
-            
         }
         dismiss(animated: true, completion: nil)
     }
-    
-    
 }
